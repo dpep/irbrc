398b47e4bb55f0c18e0dcb1021040dfda390df0f
dpepper <pepper.daniel@gmail.com>
2017-12-10 18:09:06 -0800

    update names / repo parsing

--- test/test.rb
+++ test/test.rb
@@ -2,25 +2,25 @@ require 'minitest/autorun'
 load 'lib/irbrc.rb'
 
 
-class IRBRCTest < Minitest::Test
+class IrbrcTest < Minitest::Test
 
   def test_parse_repo
     str = "origin git@github.com:dpep/rb_irbrc.git (fetch)"
     assert_equal(
-      IRBRC.parse_repo(str),
+      Irbrc.parse_repo(str),
       {
         source: 'github',
-        repo: 'dpep.rb_irbrc.git',
+        repo: 'dpep/rb_irbrc.git',
       }
     )
 
 
     str = "origin  git@bitbucket.org:danielpepper/bit123.git (fetch)"
     assert_equal(
-      IRBRC.parse_repo(str),
+      Irbrc.parse_repo(str),
       {
         source: 'bitbucket',
-        repo: 'danielpepper.bit123.git',
+        repo: 'danielpepper/bit123.git',
       }
     )
   end
